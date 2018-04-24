65f64a9aa79a4cb6504cd49101927573a87b04e3
Matthew Hu <matthu24@gmail.com>
2018-04-24 14:59:41 -0700

    http

--- frontend/utils/weather.js
+++ frontend/utils/weather.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export const fetchWeather = (zip) => {
   const API_KEY = '5ab0e54f94cd4b682ebe2cdb1675cc56';
-  const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+  const ROOT_URL = `//api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
   const url = `${ROOT_URL}&zip=${zip},us&units=imperial`;
   return axios.get(url);
 }
